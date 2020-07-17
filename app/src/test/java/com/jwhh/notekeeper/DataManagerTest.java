d87969355e7eb918ac343a4c1a98855ba1890266
tana <charleatanasi@gmail.com>
2020-07-17 20:47:01 +0300

    Added NoteCreationTest and NoteListActivityTest classes

--- app/src/test/java/com/jwhh/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/jwhh/notekeeper/DataManagerTest.java
@@ -1,25 +1,72 @@
 package com.jwhh.notekeeper;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
 
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        sDataManager = DataManager.getInstance();
+    }
+
     @Test
     public void createNewNote() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body for my test note";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, compareNote.getCourse());
+        assertEquals(noteTitle, compareNote.getTitle());
+        assertEquals(noteText, compareNote.getText());
+    }
+
+    @Test
+    public void findSimilarNotes() {
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body of test note";
+        final String noteText2 = "This is body of second test note";
+
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        int foundIndex1 = sDataManager.findNote(newNote1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = sDataManager.findNote(newNote2);
+        assertEquals(noteIndex2, foundIndex2);
+    }
+
+    @Test
+    public void createNewNoteOneStepCreation() {
+        CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of my test note";
+
+        int noteIndex = sDataManager.createNewNote(course, noteTitle, noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
